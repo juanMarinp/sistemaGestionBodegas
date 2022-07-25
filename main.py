5bf45e0fa39f0e5dfedcd9cf7d965fc615815c1b
JuanMarinP <juanjomarin368@gmail.com>
2022-07-25 12:29:19 -0400

    Al iniciar sesión se darán 3 intentos para ingresar un usuario y contraseña correctos

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ import login
 import newUser
 
 access = False
+accessContador = 0
 
-if login.login():
-    print('|...Contraseña correcta...|')
-    access = True
-else:
-    print('|...Contraseña o usuario incorrectos...|')
-    access = False
-
-#newUser.newUser()
+while access == False:
+    if login.login():
+        print('|...Contraseña correcta...|')
+        access = True
+    else:
+        print('|...Contraseña o usuario incorrectos...|')
+        access = False
+        accessContador += 1
+    if accessContador == 3:
+        print('|...Ha excedido el máximo de intentos...|')
+        break
