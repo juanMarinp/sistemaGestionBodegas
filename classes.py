154411d2c69252c2a25a8506596ee278c4323eea
JuanMarinP <juanjomarin368@gmail.com>
2022-07-25 21:58:57 -0400

    se elimió el atributo id_bodega de la clase producto

--- classes.py
+++ classes.py
@@ -1,6 +1,5 @@
 class Jefe:
     def __init__(self, rut, nombre, apellido, id_bodega, usuario, contrasenia, id_cargo):
-
         self.rut = rut
         self.nombre = nombre
         self.apellido = apellido
@@ -11,7 +10,6 @@ class Jefe:
 
 class Bodeguero:
     def __init__(self, rut, nombre, apellido, id_bodega, usuario, contrasenia, id_cargo, id_jefe):
-
         self.rut = rut
         self.nombre = nombre
         self.apellido = apellido
@@ -20,11 +18,11 @@ class Bodeguero:
         self.contrasenia = contrasenia
         self.id_cargo = id_cargo
         self.id_jefe = id_jefe
+
 class Producto:
-    def __init__(self, ident, id_tipo, id_editorial, descripcion, id_bodega, titulo):
+    def __init__(self, ident, id_tipo, id_editorial, descripcion, titulo):
         self.ident = ident
         self.id_tipo = id_tipo
         self.id_editorial = id_editorial
         self.descripcion = descripcion
-        self.id_bodega = id_bodega
         self.titulo = titulo
